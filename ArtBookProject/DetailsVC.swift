449bc873f2676706f742513fe9dce35005a92fda
cardaozel <arda.civan@gmail.com>
2024-11-29 01:37:45 +0300

    create the DetailsVC

---
+++ ArtBookProject/DetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsVC.swift
+//  ArtBookProject
+//
+//  Created by Arda Özel on 29.11.2024.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
